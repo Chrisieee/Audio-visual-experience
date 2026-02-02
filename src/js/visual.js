44fda9cb5d19012117b380c47f96a40586e98597
chrisieee <christa_pol1@hotmail.com>
2026-02-02 13:09:43 +0100

    Code beetje opgeruimd

--- src/js/visual.js
+++ src/js/visual.js
@@ -9,12 +9,19 @@ const context1 = canvas1.getContext("2d")
 canvas1.width = window.innerWidth / 2
 canvas1.height = window.innerHeight / 2
 
+// spullen om de audio uit elkaar te kunnen halen en te analyseren
 const audio = document.querySelector("#audio")
 const audioContext = new AudioContext()
 const track = audioContext.createMediaElementSource(audio)
 const analyser = audioContext.createAnalyser()
 
-// speeld af als je op de knop drukt
+track.connect(analyser)
+analyser.connect(audioContext.destination)
+
+analyser.fftSize = 256
+const data = new Uint8Array(analyser.frequencyBinCount)
+
+// speelt af als je op de knop drukt
 const button = document.querySelector("button")
 button.addEventListener("click", buttonHandler)
 
@@ -35,20 +42,7 @@ function buttonHandler() {
     }
 }
 
-let mouse = {x: 0, y: 0}
-
-window.addEventListener("mousemove", e => {
-    mouse.x = e.clientX
-    mouse.y = e.clientY
-})
-
-track.connect(analyser)
-analyser.connect(audioContext.destination)
-
-analyser.fftSize = 256
-const data = new Uint8Array(analyser.frequencyBinCount)
-
-//animatie om de audio in een cirkel te krijgen
+//basis animatie om de audio in een cirkel te krijgen
 function animate() {
     requestAnimationFrame(animate)
     analyser.getByteFrequencyData(data)
@@ -82,6 +76,7 @@ function animate() {
     })
 }
 
+// animatie met een glitch effect
 function animate1() {
     requestAnimationFrame(animate1)
     analyser.getByteFrequencyData(data)
@@ -96,6 +91,7 @@ function animate1() {
         const angle = -(i / data.length) * Math.PI * 2 + Math.PI
         const length = value * 0.56
 
+        // kijkt naar gemiddelde bas
         let bass = 0;
         for (let i = 0; i < 10; i++) {
             bass += data[i]
@@ -103,12 +99,10 @@ function animate1() {
         bass /= 10;
         let smoothBass = 0;
         smoothBass += (bass - smoothBass) * 0.1
-        
-        let jitter = 0
 
-        if (smoothBass > 21) {
-            jitter = (Math.random() - 0.5) * 5
-        }
+        // hoe meer bas hoe erger het glitch effect
+        const intensity = Math.max(0, smoothBass - 15)
+        const jitter = intensity * 0.3 * (Math.random() - 0.5)
 
         const x = centerX + Math.cos(angle) * (radius + length) + jitter
         const y = centerY + Math.sin(angle) * (radius + length) + jitter
